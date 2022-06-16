ba70d3e3b7fb9f56663256919f4f6cb8b686db2e
markkua <markkua@live.com>
2022-06-16 12:13:28 +0200

    [UPDATE] add video

--- code/src/components/content/WhatCanWeDo.vue
+++ code/src/components/content/WhatCanWeDo.vue
@@ -3,6 +3,15 @@
 
   <div>
     <h4>What can we do as individuals</h4>
+
+
+    <h6>
+      For a more detailed explanation of carbon neutrality, please check this video:
+    </h6>
+    <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/9pPsso2acew"
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen></iframe>
   </div>
 
 </template>
@@ -10,8 +19,7 @@
 <script>
 export default {
   name: "WhatCanWeDo",
-  components: {
-  }
+  components: {}
 }
 </script>
 
